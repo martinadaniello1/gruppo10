ea0c3291f900fe3c8bfc91e8ff89acdae0fa63f1
adc47 <a.dellacorte19@gmail.com>
2023-11-21 14:58:42 +0100

    struttura del progetto

--- AutomateHub/src/automatehub/controller/FXMLDocumentController.java
+++ AutomateHub/src/automatehub/controller/FXMLDocumentController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
  */
-package automatehub;
+package automatehub.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- AutomateHub/src/automatehub/model/AutomateHub.java
+++ AutomateHub/src/automatehub/model/AutomateHub.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
  */
-package automatehub;
+package automatehub.model;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
