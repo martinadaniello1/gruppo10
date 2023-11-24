6a62f8a53cc2112587eaecbd68c853c22bb6be0d
martina <127332462+martinadaniello1@users.noreply.github.com>
2023-11-24 13:09:28 +0100

    Update AutomateHub.java

--- AutomateHub/src/automatehub/model_view/AutomateHub.java
+++ AutomateHub/src/automatehub/model_view/AutomateHub.java
@@ -24,12 +24,7 @@ public class AutomateHub extends Application {
         
         stage.setScene(scene);
         stage.show();
-        
-        RuleManagerService ruleManager = null;
-        ruleManager = RuleManagerService.getRuleManager();
-        //ruleManager.setOnSucceeded();
-        
-        ruleManager.start();
+    
         
         
     }
