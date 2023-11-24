2d17933627634513fc1ce2c6cabbff8f4fb18568
martinadaniello1 <martinadaniello01@gmail.com>
2023-11-24 14:49:13 +0100

    aggiungo Action, rimosso per sbaglio

---
+++ AutomateHub/src/automatehub/model_view/Action.java
@@ -0,0 +1,13 @@
+package automatehub.model_view;
+
+/**
+ *
+ * @author mapic
+ */
+
+public interface Action {
+           
+    public int execute();
+    public void setNameAction(String nameAction);    
+    public String getNameAction();    
+}
