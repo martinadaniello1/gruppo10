9d25789f4b45e1e03857384cc66c337f1a68e61c
martinadaniello1 <martinadaniello01@gmail.com>
2023-11-24 13:05:26 +0100

    Revert "task 102.02" revert to 3290102e6830c476de1f3c588c7dc9809e69b85c
    This reverts commit 3290102e6830c476de1f3c588c7dc9809e69b85c.

--- AutomateHub/src/automatehub/model_view/AutomateHub.java
+++ AutomateHub/src/automatehub/model_view/AutomateHub.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
 package automatehub.model_view;
 
 import javafx.application.Application;
@@ -20,6 +24,13 @@ public class AutomateHub extends Application {
         
         stage.setScene(scene);
         stage.show();
+        
+        RuleManagerService ruleManager = null;
+        ruleManager = RuleManagerService.getRuleManager();
+        //ruleManager.setOnSucceeded();
+        
+        ruleManager.start();
+        
         
     }
 
