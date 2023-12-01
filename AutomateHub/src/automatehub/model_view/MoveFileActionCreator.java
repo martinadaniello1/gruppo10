299e5516586125b6f57ebecf5dae7db7f1968c44
adc47 <a.dellacorte19@gmail.com>
2023-12-01 16:16:09 +0100

    Copy and Move a file class upload

---
+++ AutomateHub/src/automatehub/model_view/MoveFileActionCreator.java
@@ -0,0 +1,23 @@
+
+package automatehub.model_view;
+
+/**
+ *
+ * @author adc01
+ */
+public class MoveFileActionCreator implements CreatorAction {
+ 
+    private String startingPath ;
+    private String destinationPath ;
+
+    public MoveFileActionCreator(String startingPath, String destinationPath) {
+        this.startingPath = startingPath;
+        this.destinationPath = destinationPath;
+    }
+       
+    @Override
+    public Action create() {
+        return new MoveFileAction(startingPath, destinationPath);
+    }
+    
+}
