aa780a3266ce80ef0cc641c50c042fe46a942513
adc47 <a.dellacorte19@gmail.com>
2023-12-09 15:20:22 +0100

    aggiornamento nomi file

--- AutomateHub/src/automatehub/model_view/DayofWeekTrigger.java
+++ AutomateHub/src/automatehub/model_view/DayofWeekTrigger.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package automatehub.model_view;
 
 import java.io.Serializable;
